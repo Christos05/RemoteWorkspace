94c29a9ec3de50c4e389793dfa64c13fbaf5410f
HeIsJohn2011 <girai@192.168.1.4>
2024-05-26 23:11:38 +0300

    delete pita

--- src/main/java/net/mcreator/chrissmod/init/ChrissModModItems.java
+++ src/main/java/net/mcreator/chrissmod/init/ChrissModModItems.java
@@ -13,7 +13,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.BlockItem;
 
 import net.mcreator.chrissmod.item.TomatoItem;
-import net.mcreator.chrissmod.item.PitaItem;
 import net.mcreator.chrissmod.item.OnionItem;
 import net.mcreator.chrissmod.ChrissModMod;
 
@@ -21,7 +20,6 @@ public class ChrissModModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, ChrissModMod.MODID);
 	public static final RegistryObject<Item> ONION_PLANT = block(ChrissModModBlocks.ONION_PLANT);
 	public static final RegistryObject<Item> TEST = block(ChrissModModBlocks.TEST);
-	public static final RegistryObject<Item> PITA = REGISTRY.register("pita", () -> new PitaItem());
 	public static final RegistryObject<Item> TOMATO = REGISTRY.register("tomato", () -> new TomatoItem());
 	public static final RegistryObject<Item> ONION = REGISTRY.register("onion", () -> new OnionItem());
 
