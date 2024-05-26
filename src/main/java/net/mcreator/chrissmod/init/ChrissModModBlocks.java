7703bc323cf35b7b4c87b12b7e03c26e0be2636c
DESKTOP-6DS6GK9\xristos <matziaris.chrhstos@gmail.com>
2024-05-26 23:42:21 +0300

    MCreator Merge commit '81fc8f2e331330a9b452a3d02756530dbc61e56f' into 6ed771410adb38472c926e4ecf624b44fb311e6c

--- src/main/java/net/mcreator/chrissmod/init/ChrissModModBlocks.java
+++ src/main/java/net/mcreator/chrissmod/init/ChrissModModBlocks.java
@@ -15,6 +15,7 @@ import net.mcreator.chrissmod.ChrissModMod;
 
 public class ChrissModModBlocks {
 	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, ChrissModMod.MODID);
+	public static final RegistryObject<Block> ONION_PLANT = REGISTRY.register("onion_plant", () -> new OnionPlantBlock());
 	public static final RegistryObject<Block> TEST = REGISTRY.register("test", () -> new TestBlock());
 	// Start of user code block custom blocks
 	// End of user code block custom blocks
--- src/main/java/net/mcreator/chrissmod/init/ChrissModModTabs.java
+++ src/main/java/net/mcreator/chrissmod/init/ChrissModModTabs.java
@@ -21,7 +21,9 @@ public class ChrissModModTabs {
 
 	@SubscribeEvent
 	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
-		if (tabData.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
+		if (tabData.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
+			tabData.accept(ChrissModModBlocks.ONION_PLANT.get().asItem());
+		} else if (tabData.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
 			tabData.accept(ChrissModModItems.TOMATO.get());
 			tabData.accept(ChrissModModItems.ONION.get());
 		}
