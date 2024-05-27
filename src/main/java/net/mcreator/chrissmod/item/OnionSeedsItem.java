1d67c510d9631edaf2d755550aa0acbe9de81139
Christos05 <matziaris.chrhstos@gmail.com>
2024-05-27 23:06:46 +0300

    Onion Seeds logic

--- src/main/java/net/mcreator/chrissmod/item/OnionSeedsItem.java
+++ src/main/java/net/mcreator/chrissmod/item/OnionSeedsItem.java
@@ -16,7 +16,7 @@ public class OnionSeedsItem extends Item {
 	@Override
 	public InteractionResult useOn(UseOnContext context) {
 		super.useOn(context);
-		OnionSeedsRightclickedOnBlockProcedure.execute();
+		OnionSeedsRightclickedOnBlockProcedure.execute(context.getLevel(), context.getClickedPos().getX(), context.getClickedPos().getY(), context.getClickedPos().getZ(), context.getPlayer());
 		return InteractionResult.SUCCESS;
 	}
 }
--- src/main/java/net/mcreator/chrissmod/procedures/OnionSeedsRightclickedOnBlockProcedure.java
+++ src/main/java/net/mcreator/chrissmod/procedures/OnionSeedsRightclickedOnBlockProcedure.java
@@ -2,14 +2,39 @@ package net.mcreator.chrissmod.procedures;
 
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.BlockPos;
 
 import net.mcreator.chrissmod.init.ChrissModModItems;
+import net.mcreator.chrissmod.init.ChrissModModBlocks;
 
 public class OnionSeedsRightclickedOnBlockProcedure {
-	public static void execute() {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
 		ItemStack Seeds = ItemStack.EMPTY;
 		BlockState OnionPlantStage0 = Blocks.AIR.defaultBlockState();
 		Seeds = new ItemStack(ChrissModModItems.ONION_SEEDS.get());
+		OnionPlantStage0 = ChrissModModBlocks.ONION_PLANT_STAGE_0.get().defaultBlockState();
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getItem() == Seeds.getItem() && (world.getBlockState(BlockPos.containing(x, y, z))).is(BlockTags.create(new ResourceLocation("crops:farmland")))
+				&& (world.getBlockState(BlockPos.containing(x, y + 1, z))).getBlock() == Blocks.AIR && OnionPlantStage0.canSurvive(world, BlockPos.containing(x, y, z))) {
+			world.setBlock(BlockPos.containing(x, y + 1, z), OnionPlantStage0, 3);
+			if (!(entity instanceof Player _plr ? _plr.getAbilities().instabuild : false)) {
+				if (entity instanceof LivingEntity _entity) {
+					ItemStack _setstack = (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).copy();
+					_setstack.setCount((int) ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getCount() - 1));
+					_entity.setItemInHand(InteractionHand.MAIN_HAND, _setstack);
+					if (_entity instanceof Player _player)
+						_player.getInventory().setChanged();
+				}
+			}
+		}
 	}
 }
