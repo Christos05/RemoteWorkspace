6866e81dd29709a5ae87c616b6a4ed9ef7d8c699
Christos05 <matziaris.chrhstos@gmail.com>
2024-05-27 21:59:09 +0300

    Farm Land Cheker Loop

---
+++ src/main/java/net/mcreator/chrissmod/procedures/FarmLandCheckLoopProcedure.java
@@ -0,0 +1,39 @@
+package net.mcreator.chrissmod.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.common.ToolActions;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.network.chat.Component;
+import net.minecraft.core.BlockPos;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class FarmLandCheckLoopProcedure {
+	@SubscribeEvent
+	public static void onUseHoe(BlockEvent.BlockToolModificationEvent event) {
+		if (!event.isSimulated() && event.getToolAction() == ToolActions.HOE_TILL && event.getPlayer() != null) {
+			execute(event, event.getContext().getLevel(), event.getContext().getClickedPos().getX(), event.getContext().getClickedPos().getY(), event.getContext().getClickedPos().getZ(), event.getPlayer());
+		}
+	}
+
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == Blocks.FARMLAND) {
+			if (entity instanceof Player _player && !_player.level().isClientSide())
+				_player.displayClientMessage(Component.literal("Player Hoed a Block"), false);
+		}
+	}
+}
