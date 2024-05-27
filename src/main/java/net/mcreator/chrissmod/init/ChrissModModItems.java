09b0f395616cc1012c6ad0a774ee6374f2b939a4
girai <girai@192.168.1.4>
2024-05-27 13:47:03 +0300

    MCreator Merge commit 'a907282f59a07b14b4dee26b5c8f8ec2376870e3' into 8592ea975cc5bbf77aa142e58e697fdc12c16ac7

--- src/main/java/net/mcreator/chrissmod/init/ChrissModModItems.java
+++ src/main/java/net/mcreator/chrissmod/init/ChrissModModItems.java
@@ -13,17 +13,15 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.BlockItem;
 
 import net.mcreator.chrissmod.item.TomatoItem;
-import net.mcreator.chrissmod.item.OnionSeedsItem;
 import net.mcreator.chrissmod.item.OnionItem;
 import net.mcreator.chrissmod.ChrissModMod;
 
 public class ChrissModModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, ChrissModMod.MODID);
+	public static final RegistryObject<Item> ONION_PLANT = block(ChrissModModBlocks.ONION_PLANT);
 	public static final RegistryObject<Item> TEST = block(ChrissModModBlocks.TEST);
 	public static final RegistryObject<Item> TOMATO = REGISTRY.register("tomato", () -> new TomatoItem());
 	public static final RegistryObject<Item> ONION = REGISTRY.register("onion", () -> new OnionItem());
-	public static final RegistryObject<Item> ONION_PLANT = block(ChrissModModBlocks.ONION_PLANT);
-	public static final RegistryObject<Item> ONION_SEEDS = REGISTRY.register("onion_seeds", () -> new OnionSeedsItem());
 
 	// Start of user code block custom items
 	// End of user code block custom items
