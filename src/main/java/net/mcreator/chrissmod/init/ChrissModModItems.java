0fde2a40fbebb530805602137734459b1fa6c52f
Christos05 <matziaris.chrhstos@gmail.com>
2024-05-27 14:24:27 +0300

    Test2 Item

--- src/main/java/net/mcreator/chrissmod/init/ChrissModModItems.java
+++ src/main/java/net/mcreator/chrissmod/init/ChrissModModItems.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.BlockItem;
 
 import net.mcreator.chrissmod.item.TomatoItem;
+import net.mcreator.chrissmod.item.Test2Item;
 import net.mcreator.chrissmod.item.OnionSeedsItem;
 import net.mcreator.chrissmod.item.OnionItem;
 import net.mcreator.chrissmod.ChrissModMod;
@@ -24,6 +25,7 @@ public class ChrissModModItems {
 	public static final RegistryObject<Item> ONION = REGISTRY.register("onion", () -> new OnionItem());
 	public static final RegistryObject<Item> ONION_PLANT = block(ChrissModModBlocks.ONION_PLANT);
 	public static final RegistryObject<Item> ONION_SEEDS = REGISTRY.register("onion_seeds", () -> new OnionSeedsItem());
+	public static final RegistryObject<Item> TEST_2 = REGISTRY.register("test_2", () -> new Test2Item());
 
 	// Start of user code block custom items
 	// End of user code block custom items
